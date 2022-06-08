8d930d1fd7f37ec6bfadec6420651a0997cd9e37
ekene12e <ekenestanekeson@gmail.com>
2022-06-08 20:44:50 +0100

    task 6

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 def print_sorted_dictionary(a_dictionary):
     if a_dictionary != {}:
-        for key, value in a_dictionary.items():
+        for key, value in a_dictionary.items().sorted():
             print(f'{key}: {value}')
