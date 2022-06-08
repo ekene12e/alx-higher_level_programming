1f1187e31e9a7b63403fe8d5130f3f9d89a2a202
ekene12e <ekenestanekeson@gmail.com>
2022-06-08 21:09:38 +0100

    task 9

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -1,9 +1,6 @@
 #!/usr/bin/python3
 def multiply_by_2(a_dictionary):
-    """returns a new dictionary with values x2"""
-    if a_dictionary != {}:
-        new_dict = {}
-        for key, value in a_dictionary.items():
-            new_dict[key] = value * 2
-        return new_dict
-
+    """returns a new dictionary with"""
+    """all values multiplied by 2"""
+    new_dict = {x: (a_dictionary[x] * 2) for x in a_dictionary}
+    return new_dict
