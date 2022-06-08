8c3188024886a3f4cb911c014a0903bad1c62a5b
ekene12e <ekenestanekeson@gmail.com>
2022-06-08 20:21:12 +0100

    task 4

--- 0x04-python-more_data_structures/4-only_diff_elements.py
+++ 0x04-python-more_data_structures/4-only_diff_elements.py
@@ -1,3 +1,10 @@
 #!/usr/bin/python3
 def only_diff_elements(set_1, set_2):
-    return list(set_1.union(set_2))
+    new_list = []
+    for item in set_1:
+        if item not in set_2 and item not in new_list:
+            new_list.append(item)
+    for item in set_2:
+        if item not in set_1 and item not in new_list:
+            new_list.append(item)
+    return new_list
