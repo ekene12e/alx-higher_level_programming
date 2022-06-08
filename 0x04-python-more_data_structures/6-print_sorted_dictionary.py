5a1a240df7d17738220728d8fdcf98995f5bd56d
ekene12e <ekenestanekeson@gmail.com>
2022-06-08 20:49:39 +0100

    task 7

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 def print_sorted_dictionary(a_dictionary):
+    """prints a sorted dictionary by their keys"""
     if a_dictionary != {}:
         for key, value in sorted(a_dictionary.items()):
             print(f'{key}: {value}')
