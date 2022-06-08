3d59ca273d35828fa5875899967666f5cf58b6d3
ekene12e <ekenestanekeson@gmail.com>
2022-06-08 21:43:50 +0100

    task 10

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
     if a_dictionary is not  None:
-        values_list = []
-        for key, value in a_dictionary:
-            values_list.append(value)
-        return max(values_list)
+        values_list = list(a_dictionary.values())
+        keys_list = list(a_dictionary.keys())
+        max_score = max(values_list)
+        max_score_index = values_list.index(max_score)
+        max_score_key = keys_list[max_score_index]
+        return max_score_key
     return None
