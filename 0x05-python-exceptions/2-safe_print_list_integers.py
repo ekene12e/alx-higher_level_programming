e64a5e958779becd1361cd829d0a5b51b3e5dc47
ekene12e <ekenestanekeson@gmail.com>
2022-06-20 17:01:52 +0100

    modified task 3

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -11,5 +11,4 @@ def safe_print_list_integers(my_list=[], x=0):
             continue
         except IndexError:
             break
-        finally:
-            return printed_int
+    return printed_int
