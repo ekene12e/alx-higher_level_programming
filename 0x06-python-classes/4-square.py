45447d77f0f9369687a9c102975ecde825ca136d
ekene12e <ekenestanekeson@gmail.com>
2022-06-21 12:50:46 +0100

    classes

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -5,45 +5,45 @@
 
 
 class Square:
-        '''
-        Defines a square
-        '''
-        def __init__(self, size=0):
-            """
-            INSTATIATES a new square
+    '''
+    Defines a square
+    '''
+    def __init__(self, size=0):
+        """
+        INSTATIATES a new square
 
-            Args:
-                size (int): size of square
-            """
-            self.__size = size
+        Args:
+            size (int): size of square
+        """
+        self.__size = size
 
-        @property
-        def size(self):
-            """private instance attribute
+    @property
+    def size(self):
+        """private instance attribute
 
-            Returns:
-                int: size of square
-            """
-            return self.__size
+        Returns:
+            int: size of square
+        """
+        return self.__size
 
-        @size.setter
-        def size(self, value):
-            """Sets the size of square
+    @size.setter
+    def size(self, value):
+        """Sets the size of square
 
-            Args:
-                value (int): value to set
+        Args:
+            value (int): value to set
 
-            Raises:
-                TypeError: error
-                ValueError: error
-            """
-            if not isinstance(value, int):
-                raise TypeError('size must be an integer')
-            if value < 0:
-                raise ValueError('size must be >= 0')
-            self.__size = value
+        Raises:
+            TypeError: error
+            ValueError: error
+        """
+        if not isinstance(value, int):
+            raise TypeError('size must be an integer')
+        if value < 0:
+            raise ValueError('size must be >= 0')
+        self.__size = value
 
-        def area(self):
-            """Returns the area of the square
-            """
-            return self.__size * self.__size
+    def area(self):
+        """Returns the area of the square
+        """
+        return self.__size * self.__size
