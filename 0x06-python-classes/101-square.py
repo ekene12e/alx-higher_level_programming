48b021aa93de6667344d7b0dda2dd3e1b300f488
ekene12e <ekenestanekeson@gmail.com>
2022-06-21 23:35:44 +0100

    advanced 3

--- 0x06-python-classes/101-square.py
+++ 0x06-python-classes/101-square.py
@@ -53,11 +53,11 @@ class Square:
     @position.setter
     def position(self, value):
         if not isinstance(value, tuple) or len(value) != 2:
-            raise TypeError('position must be a tuple of 2 positive integers')
+            raise TypeError('position must be a tuple of 2 positive integer')
         if not (isinstance(value[0], int) and isinstance(value[1], int)):
-            raise TypeError('position must be a tuple of 2 positive integers')
+            raise TypeError('position must be a tuple of 2 positive integer')
         if not (value[0] >= 0 and value[1] >= 0):
-            raise TypeError('position must be a tuple of 2 positive integers')
+            raise TypeError('position must be a tuple of 2 positive integer')
 
         self.__position = value
 
@@ -72,10 +72,14 @@ class Square:
         """
         if self.__size == 0:
             print()
-            return
 
-        for i in range(self.__position[1]):
-            print()
+        else:
+            for i in range(self.__position[1]):
+                print()
 
-        for i in range(self.__size):
-            print(" " * self.__position[0] + "#" * self.__size)
+            for i in range(self.__size):
+                print(" " * self.__position[0] + "#" * self.__size)
+
+    def __str__(self):
+       self.my_print()
+       return ""
