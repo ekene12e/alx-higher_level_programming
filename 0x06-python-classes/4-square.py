49c16cfecb8d8d8729b1ce4e48ec25c069154b18
ekene12e <ekenestanekeson@gmail.com>
2022-06-21 12:34:31 +0100

    classes

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -5,23 +5,44 @@
 
 
 class Square:
-    '''
-    Defines a square
-    '''
-    def __init__(self, size=0):
-        """
-        INSTATIATES a new square
+        '''
+        Defines a square
+        '''
+        def __init__(self, size=0):
+            """
+            INSTATIATES a new square
 
-        Args:
-            size (int): size of square
-        """
-        if not isinstance(size, int):
-            raise TypeError('size must be an integer')
-        if size < 0:
-            raise ValueError('size must be >= 0')
-        self.__size = size
+            Args:
+                size (int): size of square
+            """
+            self.__size = size
 
-    def area(self):
-        """Returns the area of the square
-        """
-        return self.__size * self.__size
+
+        @property
+        def size(self):
+            return self.__size
+
+        @size.setter
+        def size(self, value):
+            if not isinstance(value, int):
+                raise TypeError('size must be an integer')
+            if value < 0:
+                raise ValueError('size must be >= 0')
+            self.__size = value
+
+
+        def area(self):
+            """Returns the area of the square
+            """
+            return self.__size * self.__size
+my_square = Square(89)
+print("Area: {} for size: {}".format(my_square.area(), my_square.size))
+
+my_square.size = 3
+print("Area: {} for size: {}".format(my_square.area(), my_square.size))
+
+try:
+    my_square.size = "5 feet"
+    print("Area: {} for size: {}".format(my_square.area(), my_square.size))
+except Exception as e:
+    print(e)
