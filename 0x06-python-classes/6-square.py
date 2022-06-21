00a55238eb0188833984fab3637168abe62f88e7
ekene12e <ekenestanekeson@gmail.com>
2022-06-21 21:55:08 +0100

    task 6

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -76,6 +76,6 @@ class Square:
 
         for i in range(self.__position[1]):
             print()
+
         for i in range(self.__size):
-            print(" " * self.__position[0], end="")
-            print("#" * self.__size)
+            print(" " * self.__position[0] + "#" * self.__size)
