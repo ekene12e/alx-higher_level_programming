bedcfaa1d080f166469d23f20b7708b5d2122b2e
ekene12e <ekenestanekeson@gmail.com>
2022-06-21 11:56:11 +0100

    classes

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+class Square:
+    '''
+    Defines a square
+    '''
+    def __init__(self, size):
+        """
+        Initialize a new square
+
+        Args:
+            size (int): size of size
+        """        
+        self.__size = size
