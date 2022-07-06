69bd1a9dbfb25fb4b075d8f8468be5308db11078
ekene12e <ekenestanekeson@gmail.com>
2022-07-06 14:43:36 +0100

    task 4

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -7,4 +7,6 @@ def inherits_from(obj, a_class):
     """ checks for a kind of class"""
 
     obj_class = type(obj)
+    if obj_class == a_class:
+        return False
     return issubclass(obj_class, a_class)
