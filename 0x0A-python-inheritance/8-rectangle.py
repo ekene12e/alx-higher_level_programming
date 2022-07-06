2d9ca574a120d2aa49621604331c5c5b90aff725
ekene12e <ekenestanekeson@gmail.com>
2022-07-06 16:51:11 +0100

    task 8

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
-"""Defines an empty class BaseGeometry."""
+
+"""Defines a class Rectangle that inherits from BaseGeometry."""
 BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 
-class Rectangle(BaseException):
-    """A Rectangle object"""
+class Rectangle(BaseGeometry):
+    """Represent a rectangle using BaseGeometry."""
 
     def __init__(self, width, height):
         """Intialize a new Rectangle.
