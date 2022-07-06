de39523cd34b2ea04ef7925414919429e86f1c08
ekene12e <ekenestanekeson@gmail.com>
2022-07-06 14:23:56 +0100

    task 1

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""A class with a print sorted function
+"""
+
+
+def is_kind_of_class(obj, a_class):
+    """ checks for a kind of class"""
+
+    return isinstance(obj, a_class)
