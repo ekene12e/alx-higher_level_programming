54edc19ab4ea54de463d47118f99f26a3fd5a290
Ekene Ezema <ekenestanekeson@gmail.com>
2022-09-07 19:11:42 +0100

    alx task

--- 0x13-javascript_objects_scopes_closures/4-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -17,5 +17,9 @@ class Rectangle {
       this.width = this.height;
       this.height = temp;
     }
+    double() {
+      this.width *= 2;
+      this.height *= 2;
+    }
   }
 module.exports = Rectangle;
