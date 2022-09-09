71c7d2c4b66e4be03a9afcff40cf0f6db8071701
Ekene Ezema <ekenestanekeson@gmail.com>
2022-09-09 22:52:24 +0100

    alx task

---
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""List all cities from the db
+Username, password, and database name given as user args
+Can only use execute() once
+Sort ascending order by cities.id
+"""
+import MySQLdb
+import sys
+
+if __name__ == '__main__':
+    user = sys.argv[1]
+    password = sys.argv[2]
+    db_name = sys.argv[3]
+    search_name = sys.argv[4]
+    db = MySQLdb.connect(host='localhost',
+                         user=user,
+                         passwd=password,
+                         db=db_name,
+                         port=3306)
+    cur = db.cursor()
+    cur.execute("""SELECT cities.id, cities.name, states.name
+         FROM states
+         INNER JOIN cities ON states.id = cities.state_id
+         ORDER BY cities.id ASC"""
+    selected_cities = cur.fetchall()
+    for city in selected_cities:
+        print(city)
+
+    cur.close()
+    db.close()
