b6885690a21eeb79c941e1a5baee6cc6f8fb7e81
Ekene Ezema <ekenestanekeson@gmail.com>
2022-09-09 22:30:40 +0100

    alx task

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""List all states where 'name' matches the argument
+But this time, one safe from MySQL injection.
+Username, password, database name, and state name given as user args
+"""
+import MySQLdb
+import sys
+
+if __name__ == '__main__':
+    user = sys.argv[1]
+    password = sys.argv[2]
+    db_name = sys.argv[3]
+    search_name = sys.argv[4]
+    db = MySQLdb.connect(host='localhost',
+                         user=user,
+                         passwd=password,
+                         db=db_name,
+                         port=3306)
+    cur = db.cursor()
+    cur.execute("""SELECT id,
+                    name FROM states
+                    WHERE name=%s
+                    ORDER BY id ASC""", search_name)
+    selected_states = cur.fetchall()
+    for state in selected_states:
+        print(state)
+
+    cur.close()
+    db.close()
