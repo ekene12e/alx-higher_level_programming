86b2829b63e86910c76016a8c3c2e019cd167769
Ekene Ezema <ekenestanekeson@gmail.com>
2022-09-09 22:17:18 +0100

    alx task

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
-"""List all states where 'name' matches the argument
-But this time, one safe from MySQL injection.
-Username, password, database name, and state name given as user args
-"""
+""" a script that lists all states from the
+database hbtn_0e_0_usa"""
 import MySQLdb
 import sys
 
@@ -10,17 +8,17 @@ if __name__ == '__main__':
     user = sys.argv[1]
     password = sys.argv[2]
     db_name = sys.argv[3]
+    search_name = sys.argv[4]
     db = MySQLdb.connect(host='localhost',
                          user=user,
                          passwd=password,
                          db=db_name,
                          port=3306)
     cur = db.cursor()
-    cmd = """SELECT id, name
-         FROM states
-         WHERE name=%s
-         ORDER BY id ASC"""
-    cur.execute(cmd, (sys.argv[4]))
+    cur.execute("""SELECT id,
+                    name FROM states
+                    WHERE name LIKE BINARY '{}'
+                    ORDER BY id ASC""".format(search_name))
     selected_states = cur.fetchall()
     for state in selected_states:
         print(state)
