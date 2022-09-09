1cc6b95eecfefe24b3c79be8a3668b41de690fa0
Ekene Ezema <ekenestanekeson@gmail.com>
2022-09-09 21:51:57 +0100

    alx task

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -16,7 +16,7 @@ if __name__ == '__main__':
     cur = db.cursor()
     cur.execute("""SELECT id,
                     name FROM states
-                    WHERE name LIKE 'N%'
+                    WHERE name LIKE BINARY 'N%'
                     ORDER BY id ASC""")
     selected_states = cur.fetchall()
     for state in selected_states:
