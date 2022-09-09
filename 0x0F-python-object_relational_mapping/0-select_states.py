403281fb9d17ae22de5983d6bf5256b2dd9cc000
Ekene Ezema <ekenestanekeson@gmail.com>
2022-09-09 21:00:19 +0100

    alx task

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+""" a script that lists all states from the
+database hbtn_0e_0_usa"""
+import MySQLdb
+import sys
+
+user = sys.argv[1]
+password = sys.argv[2]
+db_name = sys.argv[3]
+db = MySQLdb.connect(host=localhost, user=user, passwd=password, db=db_name)
+cur = db.cursor()
+cur.execute("SELECT name FROM states ORDER BY states.id")
