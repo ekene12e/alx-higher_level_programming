d03a7c90b2d339aef428c9aa7f3f51101ef3f4d7
Ekene Ezema <ekenestanekeson@gmail.com>
2022-09-09 21:23:07 +0100

    alx task

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+""" a script that lists all states from the
+database hbtn_0e_0_usa"""
+import MySQLdb
+import sys
+
+user = sys.argv[1]
+password = sys.argv[2]
+db_name = sys.argv[3]
+db = MySQLdb.connect(host='localhost',
+                    user=user,
+                    passwd=password,
+                    db=db_name,
+                    port=3306)
+cur = db.cursor()
+cur.execute("SELECT id, name FROM states WHERE state LIKE 'B%'\
+    ORDER BY id ASC")
+selected_states = cur.fetchall()
+
+for state in selected_states:
+    print(state)
+
+cur.close()
+db.close()
+
