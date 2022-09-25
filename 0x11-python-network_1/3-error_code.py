b794818f9190b7f2abdbb824ca9d3bb9510bd950
Ekene Ezema <ekenestanekeson@gmail.com>
2022-09-25 14:10:32 +0100

    alx task

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -8,7 +8,7 @@ import sys
 if __name__ == '__main__':
     req = Request(sys.argv[1])
     try:
-        with urlopen(sys.argv[1]) as res:
+        with urlopen(req) as res:
             msg = res.read()
             print(msg.decode('utf-8'))
     except URLError as e:
