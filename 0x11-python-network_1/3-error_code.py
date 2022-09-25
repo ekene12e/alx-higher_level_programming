a07ac47d865367f49247b348f786e7c63871864a
Ekene Ezema <ekenestanekeson@gmail.com>
2022-09-25 14:14:39 +0100

    alx task

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 """Takes in a url and print
 a content of the header"""
-from urllib.request import Request, urlopen, URLError
+from urllib.request import Request, urlopen
+from urllib.error import URLError
 import sys
 
 
@@ -9,8 +10,7 @@ if __name__ == '__main__':
     req = Request(sys.argv[1])
     try:
         with urlopen(req) as res:
-            msg = res.read()
-            print(msg.decode('utf-8'))
+            print(res.read().decode('utf-8'))
     except URLError as e:
         if hasattr(e, 'code'):
-            print('Error code: ', e.code)
+            print(f'Error code: {e.code}')
