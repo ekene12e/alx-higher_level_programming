9cc2a75f93e3efc466f6fb8280ab2000e51f5010
Ekene Ezema <ekenestanekeson@gmail.com>
2022-09-25 13:04:31 +0100

    alx task

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -7,7 +7,7 @@ import urllib.request
 if __name__ == '__main__':
     with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as r:
         res = r.read()
-    print(f'Body respose:')
+    print(f'Body response:')
     print(f'\t- type: {type(res)}')
     print(f'\t- content: {res}')
     print(f"\t- utf8 content: {res.decode('utf8')}")
