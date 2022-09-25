fb9fcc77c59f7abb9b874b9678c17bd24697296e
Ekene Ezema <ekenestanekeson@gmail.com>
2022-09-25 14:05:15 +0100

    alx task

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -7,13 +7,13 @@ import sys
 
 
 if __name__ == '__main__':
-	req = Request(sys.argv[1])
-	try:
-		with urlopen(sys.argv[1]) as res:
-			msg = res.read()
-			print(msg.decode('utf-8'))
-	except URLError as e:
-		if hasattr(e, 'reason'):
-			print('Reason: ', e.reason)
-		elif hasattr(e, 'code'):
-			print('Error code: ', e.code)
+    req = Request(sys.argv[1])
+    try:
+        with urlopen(sys.argv[1]) as res:
+            msg = res.read()
+            print(msg.decode('utf-8'))
+    except URLError as e:
+        if hasattr(e, 'reason'):
+            print('Reason: ', e.reason)
+        elif hasattr(e, 'code'):
+            print('Error code: ', e.code)
