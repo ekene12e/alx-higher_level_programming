2a39d7682a201de57663c537b585a9cf71d2ecc9
Ekene Ezema <ekenestanekeson@gmail.com>
2022-09-25 14:01:55 +0100

    alx task

---
+++ 0x11-python-network_1/3-error_code.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""Takes in a url and print
+a content of the header"""
+from email import message
+from urllib.request import Request, urlopen, URLError
+import sys
+
+
+if __name__ == '__main__':
+	req = Request(sys.argv[1])
+	try:
+		with urlopen(sys.argv[1]) as res:
+			msg = res.read()
+			print(msg.decode('utf-8'))
+	except URLError as e:
+		if hasattr(e, 'reason'):
+			print('Reason: ', e.reason)
+		elif hasattr(e, 'code'):
+			print('Error code: ', e.code)
