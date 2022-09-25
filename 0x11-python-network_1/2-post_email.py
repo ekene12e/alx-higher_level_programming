337d70f4ab85b59a74c105a3de638ccb55494925
Ekene Ezema <ekenestanekeson@gmail.com>
2022-09-25 13:40:08 +0100

    alx task

---
+++ 0x11-python-network_1/2-post_email.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Takes in a url and email address
+then posts the email to the url as data"""
+import urllib.request
+import sys
+
+if __name__ == '__main__':
+    data = {'email': sys.argv[2]}
+    data = urllib.parse.urlencode(data)
+    data = data.encode('ascii')
+    req = urllib.request.Request(sys.argv[1], data)
+    with urllib.request.urlopen(req, data) as res:
+        message = res.read()
