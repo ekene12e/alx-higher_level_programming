07a09ebe5e027d59600cff0306bc68b7a57c77c6
Ekene Ezema <ekenestanekeson@gmail.com>
2022-09-25 16:07:32 +0100

    alx task

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -11,5 +11,5 @@ if __name__ == '__main__':
     password = sys.argv[2]
     result = requests.get('https://api.github.com/user',
                           auth=(username, password))
-    my_id = result.json()['id']
+    my_id = result.json().get('id')
     print(my_id)
