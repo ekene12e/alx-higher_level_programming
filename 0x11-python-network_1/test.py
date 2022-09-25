a5bbfb01768eea779bcc6d45f38d90211d9d3a70
ekene12e <ekenestanekeson@gmail.com>
2022-09-25 19:27:37 +0100

    removed test file

--- 0x11-python-network_1/test.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python3
-import requests
-
-
-if __name__ == '__main__':
-    result = requests.get('https://api.github.com/user', auth=('ekene12e', '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'))
-    my_id = result.json()['id']
-    print(my_id)
